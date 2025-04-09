86275c56d960ee9c6cabf907afc127166c793d1f
TheKartikeyaMohan <75328564+TheKartikeyaMohan@users.noreply.github.com>
2025-04-10 04:32:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import { execFile } from "child_process";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8080;
+
 
 app.use(cors());
 app.use(express.json());
